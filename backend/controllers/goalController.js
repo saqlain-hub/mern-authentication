e4bc9c7e6b98a85a2ec20339c7db26a48b2387a8
Saqlain Javed <saqlainsj12@gmail.com>
2024-01-16 02:16:32 +0500

    routes and controllers

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,32 @@
+// @desc    Get goals
+// @route   GET /api/goals
+// @access  Private
+const getGoals = (req, res) => {
+  res.status(200).json({ message: "Get goals" });
+};
+
+// @desc    Set goals
+// @route   POST /api/goals
+// @access  Private
+const setGoal = (req, res) => {
+  res.status(200).json({ message: "Set goal" });
+};
+// @desc    Update goals
+// @route   PUT /api/goals/:id
+// @access  Private
+const updateGoal = (req, res) => {
+  res.status(200).json({ message: `Update goal ${req.params.id}` });
+};
+// @desc    Delete goal
+// @route   Delete /api/goals
+// @access  Private
+const deleteGoal = (req, res) => {
+  res.status(200).json({ message: `Delete goal ${req.params.id}` });
+};
+
+module.exports = {
+  getGoals,
+  setGoal,
+  updateGoal,
+  deleteGoal,
+};
